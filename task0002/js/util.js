53de0841ef9feb6425b78189a8a410bb84c38a7b
Rong08077 <rong08077@126.com>
2016-09-04 23:18:46 +0800

    add

--- task0002/js/util.js
+++ task0002/js/util.js
@@ -18,3 +18,60 @@ function isFunction(fn) {
 // task 2.1.2  
 // 使用递归来实现一个深度克隆，可以复制一个目标对象，返回一个完整拷贝
 // 被复制的对象类型会被限制为数字、字符串、布尔、日期、数组、Object对象。不会包含函数、正则对象等
+function cloneObject(src) {
+	var clone =src;
+
+	// Date
+	if (src instanceof Date) {
+		// 使用new 调用基本包装类型的构造函数 ，创建对象
+        clone = new Date(src.getDate());
+        return clone;
+    }
+
+    // Array
+    // for in 遍历Array，变量key为Array元素的索引
+    if (src instanceof Array) {
+            clone = [];
+            for (var key in src) {
+                clone[key] = cloneObject(src[key]);
+            }
+            return clone;
+    }
+
+    // Object
+    // for in 遍历Object，变量key为Object的属性名
+    if (src instanceof Object) {
+        clone = {};
+        for (var key in src) {
+            if (src.hasOwnProperty(key)) {       // 忽略掉继承属性
+                clone[key] = cloneObject(src[key]);
+            }
+        }
+        return clone;
+    }
+
+    return src;
+}
+
+// 测试用例：
+/*
+var srcObj = {
+    a: 1,
+    b: {
+        b1: ["hello", "hi"],
+        b2: "JavaScript"
+    }
+};
+var abObj = srcObj;
+var tarObj = cloneObject(srcObj);
+
+srcObj.a = 2;
+srcObj.b.b1[0] = "Hello";
+
+console.log(abObj.a);
+console.log(abObj.b.b1[0]);
+
+console.log(tarObj.a);      // 1
+console.log(tarObj.b.b1[0]);    // "hello"
+*/
+

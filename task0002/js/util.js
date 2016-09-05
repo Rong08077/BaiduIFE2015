abe35aa0cb69408290ce5026c5487afcedba0812
Rong08077 <rong08077@126.com>
2016-09-05 23:27:51 +0800

    add

--- task0002/js/util.js
+++ task0002/js/util.js
@@ -1,6 +1,7 @@
 /**
  * Created by Rong08077 on 16/09/03
  */
+
 // task 2.1.1 
 // 判断arr是否为一个数组，返回一个bool值
 function isArray(arr) {
@@ -75,3 +76,88 @@ console.log(tarObj.a);      // 1
 console.log(tarObj.b.b1[0]);    // "hello"
 */
 
+// task 2.1.3 
+// 对数组进行去重操作，只考虑数组中元素为数字或字符串，返回一个去重后的数组
+function uniqArray(source) {
+    var len = source.length,
+        result = source.slice(0),
+        i, datum;
+
+
+    // 从后往前双重循环比较
+    // 如果两个元素相同，删除后一个
+    while (--len > 0) {
+        datum = result[len];//
+        i = len;
+        while (i--) {
+            if (datum === result[i]) {
+                result.splice(len, 1);
+                break;
+            }
+        }
+    }
+
+    return result;
+}
+
+// hash
+function uniqArray1(arr) {
+    var obj = {};
+    var result = [];
+    for (var i = 0, len = arr.length; i < len; i++) {
+
+        var key = arr[i];
+
+        if (!obj[key]) {
+            result.push(key);
+            obj[key] = true;
+        }
+    }
+    return result;
+}
+
+
+// hash + es5
+// 速度最快
+function uniqArray2(arr) {
+    var obj = {};
+    for (var i = 0, len = arr.length; i < len; i++) {
+        obj[arr[i]] = true;
+    }
+    return Object.keys(obj);
+}
+
+function uniqArray3(arr) {
+    var new_array = [];
+    for (var i = 0, len = arr.length; i < len; i++) {
+        if (arr[i] !== '' && new_array.indexOf(arr[i]) < 0 ) {    // indexOf方法不支持IE9以下
+            new_array.push(arr[i]);
+        }
+    }
+    return new_array;
+}
+
+// 使用示例
+/*
+var al = 10000;
+var a = [];
+while (al--){
+a.push(al%2);
+}
+
+console.time('uniqArray')
+console.log(uniqArray(a).length);
+console.timeEnd('uniqArray')
+
+console.time('uniqArray1')
+console.log(uniqArray1(a).length);
+console.timeEnd('uniqArray1')
+
+console.time('uniqArray2')
+console.log(uniqArray2(a).length);
+console.timeEnd('uniqArray2')
+
+console.time('uniqArray3')
+console.log(uniqArray3(a).length);
+console.timeEnd('uniqArray3')
+*/

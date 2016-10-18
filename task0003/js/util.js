7b09c5687c06fa59d8ec95870d834b1a52946483
Rong08077 <rong08077@126.com>
2016-10-18 23:06:58 +0800

    begin task0003

---
+++ task0003/js/util.js
@@ -0,0 +1,550 @@
+/**
+ * Created by Rong08077 on 16/09/03
+ */
+
+// task 2.1.1 
+// 判断arr是否为一个数组，返回一个bool值
+function isArray(arr) {
+	// 方法1 ES 5
+    // return Array.isArray(arr);
+    // 方法2
+    return Object.prototype.toString.call(arr) === '[object Array]';
+}
+
+// 判断fn是否为一个函数，返回一个bool值
+function isFunction(fn) {
+    return Object.prototype.toString.call(fn) === '[object Function]';
+}
+
+// task 2.1.2  
+// 使用递归来实现一个深度克隆，可以复制一个目标对象，返回一个完整拷贝
+// 被复制的对象类型会被限制为数字、字符串、布尔、日期、数组、Object对象。不会包含函数、正则对象等
+function cloneObject(src) {
+	var clone =src;
+
+	// Date
+	if (src instanceof Date) {
+		// 使用new 调用基本包装类型的构造函数 ，创建对象
+        clone = new Date(src.getDate());
+        return clone;
+    }
+
+    // Array
+    // for in 遍历Array，变量key为Array元素的索引
+    if (src instanceof Array) {
+            clone = [];
+            for (var key in src) {
+                clone[key] = cloneObject(src[key]);
+            }
+            return clone;
+    }
+
+    // Object
+    // for in 遍历Object，变量key为Object的属性名
+    if (src instanceof Object) {
+        clone = {};
+        for (var key in src) {
+            if (src.hasOwnProperty(key)) {       // 忽略掉继承属性
+                clone[key] = cloneObject(src[key]);
+            }
+        }
+        return clone;
+    }
+
+    return src;
+}
+
+// 测试用例：
+/*
+var srcObj = {
+    a: 1,
+    b: {
+        b1: ["hello", "hi"],
+        b2: "JavaScript"
+    }
+};
+var abObj = srcObj;
+var tarObj = cloneObject(srcObj);
+
+srcObj.a = 2;
+srcObj.b.b1[0] = "Hello";
+
+console.log(abObj.a);
+console.log(abObj.b.b1[0]);
+
+console.log(tarObj.a);      // 1
+console.log(tarObj.b.b1[0]);    // "hello"
+*/
+
+// task 2.1.3 
+// 对数组进行去重操作，只考虑数组中元素为数字或字符串，返回一个去重后的数组
+function uniqArray(source) {
+    var len = source.length,
+        result = source.slice(0),
+        i, datum;
+
+
+    // 从后往前双重循环比较
+    // 如果两个元素相同，删除后一个
+    while (--len > 0) {
+        datum = result[len];//
+        i = len;
+        while (i--) {
+            if (datum === result[i]) {
+                result.splice(len, 1);
+                break;
+            }
+        }
+    }
+
+    return result;
+}
+
+// hash
+function uniqArray1(arr) {
+    var obj = {};
+    var result = [];
+    for (var i = 0, len = arr.length; i < len; i++) {
+
+        var key = arr[i];
+
+        if (!obj[key]) {
+            result.push(key);
+            obj[key] = true;
+        }
+    }
+    return result;
+}
+
+// 速度最快
+function uniqArray2(arr) {
+    var obj = {};
+    for (var i = 0, len = arr.length; i < len; i++) {
+        obj[arr[i]] = true;
+    }
+    // ES5 返回 对象 所有可枚举 自身属性 的属性名组成的数组
+    return Object.keys(obj);
+}
+
+function uniqArray3(arr) {
+    var new_array = [];
+    for (var i = 0, len = arr.length; i < len; i++) {
+        if (arr[i] !== '' && new_array.indexOf(arr[i]) < 0 ) {    // indexOf方法不支持IE9以下
+            new_array.push(arr[i]);
+        }
+    }
+    return new_array;
+}
+
+// 使用示例
+/*
+var al = 10000;
+var a = [];
+while (al--){
+a.push(al%2);
+}
+
+console.time('uniqArray')
+console.log(uniqArray(a).length);
+console.timeEnd('uniqArray')
+
+console.time('uniqArray1')
+console.log(uniqArray1(a).length);
+console.timeEnd('uniqArray1')
+
+console.time('uniqArray2')
+console.log(uniqArray2(a).length);
+console.timeEnd('uniqArray2')
+
+console.time('uniqArray3')
+console.log(uniqArray3(a).length);
+console.timeEnd('uniqArray3')
+*/
+
+// 对字符串头尾进行空格字符的去除、包括全角半角空格、Tab等，返回一个字符串
+// 尝试使用一行简洁的正则表达式完成该题目
+function trim(str) {
+    return str.replace(/^\s+|\s+$/g, '');
+}
+
+// 使用示例
+/*
+var str = '   hi!  ';
+str = trim(str);
+console.log(str); // 'hi!'
+*/
+
+// 实现一个遍历数组的方法，针对数组中每一个元素执行fn函数，并将数组索引和元素作为参数传递
+function each(arr, fn) {
+    for (var i = 0, len = arr.length; i < len; i++) {
+        fn(arr[i],1);
+    }
+}
+
+// 其中fn函数可以接受两个参数：item和index
+// 使用示例
+/*
+var arr = ['java', 'c', 'php', 'html'];
+function output(item) {
+    console.log(item)
+}
+each(arr, output);  // java, c, php, html
+
+// 使用示例
+var arr = ['java', 'c', 'php', 'html'];
+function output(item, index) {
+    console.log(index + ': ' + item)
+}
+each(arr, output);  // 0:java, 1:c, 2:php, 3:html
+*/
+
+// 获取一个对象里面第一层元素的数量，返回一个整数
+function getObjectLength(obj) {
+    var element = 0;
+    for (var key in obj) {
+        if (obj.hasOwnProperty(key)) {
+            element++;
+        }
+    }
+    return element;
+}
+
+// 使用示例
+/*
+var obj = {
+    a: 1,
+    b: 2,
+    c: {
+        c1: 3,
+        c2: 4
+    }
+};
+console.log(getObjectLength(obj)); // 3
+*/
+
+// task 2.1.4
+// 判断是否为邮箱地址
+function isEmail(emailStr) {
+    // return (/^[a-z0-9]([-_\.]?[a-z0-9]+)*@([-_]?[a-z0-9]+)+[\.][a-z]{2,7}([\.][a-z]{2})?$/i).test(emsilStr);
+    return emailStr.search(/^[a-z0-9]([-_\.]?[a-z0-9]+)*@([-_]?[a-z0-9]+)+[\.][a-z]{2,7}([\.][a-z]{2})?$/i) !== -1;
+}
+
+// 判断是否为手机号
+function isMobilePhone(phone) {
+    phone = phone + '';
+    // return (/^(13[0-9]|15[012356789]|17[678]|18[0-9]|14[57])[0-9]{8}$/).test(phone);
+    return phone.search(/^(13[0-9]|15[012356789]|17[678]|18[0-9]|14[57])[0-9]{8}$/) !== -1;
+}
+
+// task 3.1
+function hasClass(element, className) {
+    var name = element.className.match(/\S+/g) || [];
+    if (name.indexOf(className) !== -1) {
+        return true;
+    }
+    return false;
+
+}
+
+// 为element增加一个样式名为newClassName的新样式
+function addClass(element, newClassName) {
+    if (!hasClass(element, newClassName)) {
+        element.className = trim(element.className + ' ' + newClassName);
+    }
+}
+
+// 移除element中的样式oldClassName
+function removeClass(element, oldClassName) {
+    if (hasClass(element, oldClassName)) {
+        element.className = trim(element.className.replace(oldClassName, ''));
+    }
+}
+
+// 判断siblingNode和element是否为同一个父元素下的同一级的元素，返回bool值
+function isSiblingNode(element, siblingNode) {
+    return element.parentNode === siblingNode.parentNode;
+}
+
+// 获取element相对于浏览器窗口的位置，返回一个对象{x, y}
+function getPosition(element) {
+    var x = 0;
+    var y = 0;
+    var current = element;
+
+    while (current !== null) {
+        x += current.offsetLeft;
+        y += current.offsetTop;
+        current = current.offsetParent;
+    }
+
+    var scrollLeft = document.body.scrollLeft + document.documentElement.scrollLeft;
+    var scrollTop = document.body.scrollTop + document.documentElement.scrollTop;
+
+    // element.getBoundingClientRect()
+
+    x -= scrollLeft;
+    y -= scrollTop;
+
+    return {
+        x: x,
+        y: y
+    }
+}
+
+// task 3.2
+// 实现一个简单的Query
+function $(selector) {
+    var ele = document;
+    var sele = selector.replace(/\s+/, ' ').split(' ');    // 去除多余的空格并分割
+
+    for (var i = 0, len = sele.length; i < len; i++) {
+        
+        switch (sele[i][0]) {    // 从子节点中查找
+            case '#':
+                ele = ele.getElementById(sele[i].substring(1));
+                break;
+            case '.':
+                ele = ele.getElementsByClassName(sele[i].substring(1))[0];
+                break;
+            case '[':
+                var valueLoc = sele[i].indexOf('=');
+                var temp = ele.getElementsByTagName('*');
+                var tLen = temp.length;
+                if (valueLoc !== -1) {
+                    var key = sele[i].substring(1, valueLoc);
+                    var value = sele[i].substring(valueLoc + 1, sele[i].length - 1);
+                    for (var j = 0; j < tLen; j++) {
+                        if (temp[j][key] === value) {
+                            ele = temp[j];
+                            break;
+                        }
+                    }
+                }
+                else {
+                    var key = sele[i].substring(1, sele[i].length - 1);
+                    for (var j = 0; j < tLen; j++) {
+                        if (temp[j][key]) {
+                            ele = temp[j];
+                            break;
+                        }
+                    }
+                }
+                break;
+            default :
+                ele = ele.getElementsByTagName(sele[i])[0];
+                break;
+        }
+    }
+
+    if (!ele) {
+        ele = null;
+    }
+
+    return ele;
+}
+/*
+// 可以通过id获取DOM对象，通过#标示，例如
+$("#adom"); // 返回id为adom的DOM对象
+
+// 可以通过tagName获取DOM对象，例如
+$("a"); // 返回第一个<a>对象
+
+// 可以通过样式名称获取DOM对象，例如
+$(".classa"); // 返回第一个样式定义包含classa的对象
+
+// 可以通过attribute匹配获取DOM对象，例如
+$("[data-log]"); // 返回第一个包含属性data-log的对象
+
+$("[data-time=2015]"); // 返回第一个包含属性data-time且值为2015的对象
+
+// 可以通过简单的组合提高查询便利性，例如
+$("#adom .classa"); // 返回id为adom的DOM所包含的所有子节点中，第一个样式定义包含classa的对象
+*/
+
+// task 4.1
+// 给一个element绑定一个针对event事件的响应，响应函数为listener
+function addEvent(element, event, listener) {
+    element.addEventListener(event, listener);
+}
+
+// 移除element对象对于event事件发生时执行listener的响应
+function removeEvent(element, event, listener) {
+    element.removeEventListener(event, listener);
+}
+
+// 实现对click事件的绑定
+function addClickEvent(element, listener) {
+    addEvent(element, 'click', listener);
+}
+
+// 实现对于按Enter键时的事件绑定
+function addEnterEvent(element, listener) {
+    addEvent(element, 'keydown', function(e) {
+        var event = e || window.event;
+        var keyCode = event.which || event.keyCode;
+        if (keyCode === 13) {
+            listener.call(element, event);
+        }
+    });
+}
+
+// 接下来我们把上面几个函数和$做一下结合，把他们变成$对象的一些方法
+$.on = addEvent;
+$.un = removeEvent;
+$.click = addClickEvent;
+$.enter = addEnterEvent;
+
+// task 4.2
+// 对一个列表里所有的<li>增加点击事件的监听
+function clickListener(event) {
+    console.log(event);
+}
+
+/*
+$.click($("#item1"), clickListener);
+$.click($("#item2"), clickListener);
+$.click($("#item3"), clickListener);
+*/
+
+/*
+each($("#list").getElementsByTagName('li'), function(li) {
+    addClickEvent(li, clickListener);
+*/
+
+// 我们通过自己写的函数，取到id为list这个ul里面的所有li，然后通过遍历给他们绑定事件。这样我们就不需要一个一个去绑定了。
+function clickListener(event) {
+    console.log(event);
+}
+
+function renderList() {
+    $("#list").innerHTML = '<li>new item</li>';
+}
+
+function init() {
+    each($("#list").getElementsByTagName('li'), function(item) {
+        $.click(item, clickListener);
+    });
+
+    $.click($("#btn"), renderList);
+}
+
+// 事件委托，解决事件处理程序过多
+function delegateEvent(element, tag, eventName, listener) {
+    addEvent(element, eventName, function (e) {
+        var event = e || window.event;
+        var target = event.target || event.srcElement;
+
+        if (target && target.tagName === tag.toUpperCase()) {
+            listener.call(target, event);
+        }
+    });
+}
+
+// 事件委托
+$.delegate = delegateEvent;
+
+// 使用示例
+/*
+$.click($("#btn"), renderList);
+$.delegate($("#list"), "li", "click", clickListener);
+*/
+
+// task 5.1
+// 判断是否为IE浏览器，返回-1或者版本号
+function isIE() {
+    return /msie (\d+\.\d+)/i.test(navigator.userAgent)
+        ? (document.documentMode || + RegExp['\x241']) : -1;
+}
+
+
+// 设置cookie
+function isValidCookieName(cookieName) {
+    return (new RegExp('^[^\\x00-\\x20\\x7f\\(\\)<>@,;:\\\\\\\"\\[\\]\\?=\\{\\}\\/\\u0080-\\uffff]+\x24'))
+        .test(cookieName);
+}
+
+function setCookie(cookieName, cookieValue, expiredays) {
+    if (!isValidCookieName(cookieName)) {
+        return;
+    }
+
+    var exdate = '';
+    if (expiredays) {
+        exdate = new Date();
+        exdate.setDate(exdate.getDate() + expiredays);
+        var expires = ';expires=' + exdate.toUTCString();     // toGMTString is deprecated and should no longer be used, it's only there for backwards compatibility, use toUTCString() instead
+    }
+    document.cookie = cookieName + '=' + encodeURIComponent(cookieValue) + expires;    // 废弃的 escape() 方法生成新的由十六进制转移序列替换的字符串. 使用 encodeURI 或 encodeURIComponent 代替
+}
+
+// 获取cookie值
+function getCookie(cookieName) {
+    if (!isValidCookieName(cookieName)) {
+        return null;
+    }
+
+    var re = new RegExp(cookieName + '=(.*?)($|;)');
+    return re.exec(document.cookie)[1] || null;
+}
+
+// task 6.1
+// 学习Ajax，并尝试自己封装一个Ajax方法。
+function ajax(url, options) {
+    // 创建对象
+    var xmlhttp;
+    if (window.XMLHttpRequest) {
+        xmlhttp = new XMLHttpRequest();
+    }
+    else {        //兼容 IE5 IE6
+        xmlhttp = new ActiveXObject('Microsoft.XMLHTTP');
+    }
+
+    // 处理data
+    if (options.data) {
+        var dataarr = [];
+        for (var item in options.data) {
+            dataarr.push(item + '=' + encodeURI(options.data[item]));
+        }
+        var data = dataarr.join('&');
+    }
+
+    // 处理type
+    if (!options.type) {
+        options.type = 'GET';
+    }
+    options.type = options.type.toUpperCase();
+
+    // 发送请求
+    if (options.type === 'GET') {
+        var myURL = '';
+        if (options.data) {
+            myURL = url + '?' + data;
+        }
+        else {
+            myURL = url;
+        }
+        xmlhttp.open('GET', myURL, true);
+        xmlhttp.send();
+    }
+    else if (options.type === 'POST') {
+        xmlhttp.open('POST', url, true);
+        xmlhttp.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+        xmlhttp.send(data);
+    }
+
+    // readyState
+    xmlhttp.onreadystatechange = function () {
+        if (xmlhttp.readyState === 4) {
+            if (xmlhttp.status === 200) {
+                if (options.onsuccess) {
+                    options.onsuccess(xmlhttp.responseText, xmlhttp.responseXML);
+                }
+            }
+            else {
+                if (options.onfail) {
+                    options.onfail();
+                }
+            }
+        }
+    }
+}
